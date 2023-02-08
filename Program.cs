bd8446a0c33f700c6f97735e077335736f498902
kamalnine <78726439+kamalnine@users.noreply.github.com>
2023-02-08 21:43:02 +0530

    Add files via upload
    Assignment 2

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Assignment2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Registration Number: ");
+        String a = Console.ReadLine();
+        Console.WriteLine("Enter Driver Name: ");
+        String b = Console.ReadLine();
+        Console.WriteLine("Enter Vehicle type: ");
+        String c = Console.ReadLine();
+        Console.Write("Enter costPerKm: ");
+        float d = float.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Vehicle v = new Vehicle();
+
+        Vehicle v1 = new Vehicle(a,b,c,d);
+
+
+
+        v1.Display();
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Vehicle
+    {
+
+        private string registrationNumber;
+        private string driverName;
+        private string vehicleType;
+        private float costPerKm;
+
+
+        public Vehicle()
+        {
+            Console.WriteLine("CAB REGISTRATION");
+        }
+        public Vehicle(String rNo,string dName,string vType,float CPK)
+        {
+        
+            registrationNumber = rNo;
+            driverName = dName;
+            vehicleType = vType;
+            costPerKm = CPK;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Vehicle Details: ");
+            Console.WriteLine("Registration Number : " + registrationNumber);
+            Console.WriteLine("Driver name: " + driverName);
+            Console.WriteLine("Vehicle Type : " + vehicleType);
+            Console.WriteLine("CostPerKm: " + costPerKm);
+
+
+        }
+
+
+    }
+}
